2727338ba141a19f25124e6310c555e344827ad9
spaget-spaget <oliverlimyk@gmail.com>
2025-06-30 13:22:41 +0800

    Unity Physics worksheets q1 to q5

---
+++ Week5UnityPhysics/Assets/Q5.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Character : MonoBehaviour
+{
+    CharacterController characterController;
+    public float speed = 6.0f;
+    public float jumpSpeed = 8.0f;
+    public float gravity = 20.0f;
+    private Vector3 moveDirection = Vector3.zero;
+    void Start()
+    {
+        characterController = GetComponent<CharacterController>();
+    }
+    void Update()
+    {
+        if (characterController.isGrounded)
+        {
+            moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+            moveDirection = transform.TransformDirection(moveDirection);
+            moveDirection *= speed;
+            if (Input.GetButton("Jump"))
+            {
+                moveDirection.y = jumpSpeed;
+            }
+        }
+        moveDirection.y -= gravity * Time.deltaTime;
+        characterController.Move(moveDirection * Time.deltaTime);
+    }
+}
